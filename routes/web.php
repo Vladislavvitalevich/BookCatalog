b52ed57bd3923dcfd3972e833b74f9cf2fde3806
SavchenkoV <vladislavvitalevich@gmail.com>
2020-11-05 16:06:30 +0200

    Create theme book with template pages

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//
+//  Front Route
+//
+Route::get('/', function () {
+    return view('book.index');
+});
 
 //
 //  Admin Route Group
